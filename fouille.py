ddecd9b137f8dc8229987bb0a2ba55d57a3de1ae
Siroukane <dr.siroukane@gmail.com>
2018-12-06 15:53:23 +0100

    Generate matrix file (users_movies) & dictionary from that file
    Generate matrix users * movies = ratings file & generate dictionary from that file

--- fouille.py
+++ fouille.py
@@ -1,80 +1,50 @@
-def generate_users_movies_ranking_matrix(movies_file, rankings_file):
+def generate_users_movies_rating_matrix_with_all_movies(movies_file, ratings_file):
     """
     :param movies_file: movies file
-    :param rankings_file: ranking file
+    :param ratings_file: rating file
 
     :return: dictionary in a way
     {
-        'user_1' : {'movie_1': ranking_movie_1, 'movie_2': ranking_movie_2, 'movie_3': ranking_movie_3, ...},
-        'user_2' : {'movie_1': ranking_movie_1, 'movie_2': ranking_movie_2, 'movie_3': ranking_movie_3, ...},
+        'user_1' : {'movie_1': rating_movie_1, 'movie_2': rating_movie_2, 'movie_3': rating_movie_3, ...},
+        'user_2' : {'movie_1': rating_movie_1, 'movie_2': rating_movie_2, 'movie_3': rating_movie_3, ...},
         ...
     }
     """
     f_movies = open(movies_file, "r")
-    f_rankings = open(rankings_file, "r")
-
-    nbr_movies = sum(1 for _ in f_movies) - 1
-    dictionary = {}
-
-    first = True
-    for line in f_rankings:
-        if not first:
-            args = line.split(',')
-            if args[0] not in dictionary.keys():
-                dictionary[args[0]] = {}
-
-            movie_index = int(args[1])
-            if movie_index <= nbr_movies:
-                dictionary[args[0]][movie_index] = float(args[2])
-        else:
-            first = False
-
-    return dictionary
-
-
-def generate_users_movies_ranking_matrix_with_all_movies(movies_file, rankings_file):
-    """
-    :param movies_file: movies file
-    :param rankings_file: ranking file
-
-    :return: dictionary in a way
-    {
-        'user_1' : {'movie_1': ranking_movie_1, 'movie_2': ranking_movie_2, 'movie_3': ranking_movie_3, ...},
-        'user_2' : {'movie_1': ranking_movie_1, 'movie_2': ranking_movie_2, 'movie_3': ranking_movie_3, ...},
-        ...
-    }
-    """
-    f_movies = open(movies_file, "r")
-    f_rankings = open(rankings_file, "r")
+    f_ratings = open(ratings_file, "r")
 
     nbr_movies = sum(1 for _ in f_movies)
     dictionary = {}
 
     old_user_key = "-1"
     next_index = 1
-    for line in f_rankings:
-        args = line.split(',')
-        if args[0] not in dictionary.keys():
-            if old_user_key != "-1":
-                for _index in range(next_index, nbr_movies):
-                    dictionary[old_user_key][_index] = -1.0
-            dictionary[args[0]] = {}
-            old_user_key = args[0]
+    first = True
+    for line in f_ratings:
+        if not first:
+            args = line.split(',')
+            if args[0] not in dictionary.keys():
+                if old_user_key != "-1":
+                    for _index in range(next_index, nbr_movies):
+                        dictionary[old_user_key][_index] = -1.0
+                dictionary[args[0]] = {}
+                old_user_key = args[0]
 
-        movie_index = int(args[1])
-        if movie_index <= nbr_movies:
-            if next_index < movie_index:
-                for _index in range(next_index, movie_index):
-                    dictionary[args[0]][_index] = -1.0
-            dictionary[args[0]][movie_index] = float(args[2])
-            next_index = movie_index + 1
+            movie_index = int(args[1])
+            if movie_index <= nbr_movies:
+                if next_index < movie_index:
+                    for _index in range(next_index, movie_index):
+                        dictionary[args[0]][_index] = -1.0
+                dictionary[args[0]][movie_index] = float(args[2])
+                next_index = movie_index + 1
+        else:
+            first = False
 
     return dictionary
 
 
-def generate_users_movies_ranking_matrix_with_all_movies_file(matrix, output_file):
+def generate_users_movies_rating_matrix_with_all_movies_file(matrix, output_file):
     """
-    :param matrix: result of generate users movies ranking matrix function
+    :param matrix: result of generate users movies rating matrix function
     :param output_file: output file
 
     :return: create output file content matrix data
@@ -89,33 +59,64 @@ def generate_users_movies_ranking_matrix_with_all_movies_file(matrix, output_fil
 
     f_output.close()
 
+
+def generate_users_movies_rating_dictionary_from_file(users_movies_rating_file):
+    """
+    :param users_movies_rating_file: users x movies = rating file
+
+    :return: dictionary in a way
+    {
+        'user_1' : {'movie_1': rating_movie_1, 'movie_2': rating_movie_2, 'movie_3': rating_movie_3, ...},
+        'user_2' : {'movie_1': rating_movie_1, 'movie_2': rating_movie_2, 'movie_3': rating_movie_3, ...},
+        ...
+    }
+    """
+    f_users_movies_rating = open(users_movies_rating_file, "r")
+
+    dictionary = {}
+    for line in f_users_movies_rating:
+        args = line.split(',')
+        if args[0] not in dictionary.keys():
+            dictionary[args[0]] = {}
+
+        for i in range(1, len(args) - 1):
+            value = float(args[i])
+            if value != -1.0:
+                dictionary[args[0]][i] = float(args[i])
+
+    return dictionary
+
+
 def distance(user1, user2):
     difference_absolue_notes = 0
     nombre_films_similaires = 0
     for movie1, note1 in user1.items():
-        if(user2.has_key(movie1)):
+        if (user2.has_key(movie1)):
             difference_absolue_notes += (note1 - user2[movie1]) ** 2
             nombre_films_similaires += 1
     return difference_absolue_notes / nombre_films_similaires
 
+
 def closest_users(user1, list_users, nb_users):
     closest = -1
     for user in list_users:
-        if(user != user1):
+        if (user != user1):
             dist = distance(user1, user)
-            if(closest < dist):
+            if (closest < dist):
                 closest = dist
     return closest
 
-dictionary = generate_users_movies_ranking_matrix("ml-20m/movies.csv", "ml-20m/ratings.csv")
-print(dictionary)
 
 # get the matrix
-# matrix = generate_users_movies_ranking_matrix_with_all_movies("ml-20m/movies.csv", "100_first_user_ratings.csv")
+matrix = generate_users_movies_rating_matrix_with_all_movies("ml-20m/movies.csv", "100_first_user_ratings.csv")
 # for key in matrix:
 #     print(key)
 #     print(matrix[key])
 #     print()
 
-# save matrix data into file
-# generate_users_movies_ranking_matrix_with_all_movies_file(matrix, "users_movies_ranking.csv")
+# generate file content matrix data
+generate_users_movies_rating_matrix_with_all_movies_file(matrix, "ml-20m/users_movies_rating_all.csv")
+
+# generate dictionary from generated file
+dic = generate_users_movies_rating_dictionary_from_file("ml-20m/users_movies_rating_all.csv")
+print(dic)
