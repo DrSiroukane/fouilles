aa9bd61404f6a22a52725de4349990a008218e43
Siroukane <dr.siroukane@gmail.com>
2018-12-06 15:09:38 +0100

    update generating dictionary users x movies

--- fouille.py
+++ fouille.py
@@ -13,6 +13,40 @@ def generate_users_movies_ranking_matrix(movies_file, rankings_file):
     f_movies = open(movies_file, "r")
     f_rankings = open(rankings_file, "r")
 
+    nbr_movies = sum(1 for _ in f_movies) - 1
+    dictionary = {}
+
+    first = True
+    for line in f_rankings:
+        if not first:
+            args = line.split(',')
+            if args[0] not in dictionary.keys():
+                dictionary[args[0]] = {}
+
+            movie_index = int(args[1])
+            if movie_index <= nbr_movies:
+                dictionary[args[0]][movie_index] = float(args[2])
+        else:
+            first = False
+
+    return dictionary
+
+
+def generate_users_movies_ranking_matrix_with_all_movies(movies_file, rankings_file):
+    """
+    :param movies_file: movies file
+    :param rankings_file: ranking file
+
+    :return: dictionary in a way
+    {
+        'user_1' : {'movie_1': ranking_movie_1, 'movie_2': ranking_movie_2, 'movie_3': ranking_movie_3, ...},
+        'user_2' : {'movie_1': ranking_movie_1, 'movie_2': ranking_movie_2, 'movie_3': ranking_movie_3, ...},
+        ...
+    }
+    """
+    f_movies = open(movies_file, "r")
+    f_rankings = open(rankings_file, "r")
+
     nbr_movies = sum(1 for _ in f_movies)
     dictionary = {}
 
@@ -38,9 +72,9 @@ def generate_users_movies_ranking_matrix(movies_file, rankings_file):
     return dictionary
 
 
-def generate_users_movies_ranking_matrix_file(matrix, output_file):
+def generate_users_movies_ranking_matrix_with_all_movies_file(matrix, output_file):
     """
-    :param matrix: matrixult of generate users movies ranking matrix function
+    :param matrix: result of generate users movies ranking matrix function
     :param output_file: output file
 
     :return: create output file content matrix data
@@ -56,12 +90,15 @@ def generate_users_movies_ranking_matrix_file(matrix, output_file):
     f_output.close()
 
 
+dictionary = generate_users_movies_ranking_matrix("ml-20m/movies.csv", "ml-20m/ratings.csv")
+print(dictionary)
+
 # get the matrix
-matrix = generate_users_movies_ranking_matrix("ml-20m/movies.csv", "100_first_user_ratings.csv")
+# matrix = generate_users_movies_ranking_matrix_with_all_movies("ml-20m/movies.csv", "100_first_user_ratings.csv")
 # for key in matrix:
 #     print(key)
 #     print(matrix[key])
 #     print()
 
 # save matrix data into file
-generate_users_movies_ranking_matrix_file(matrix, "users_movies_ranking.csv")
+# generate_users_movies_ranking_matrix_with_all_movies_file(matrix, "users_movies_ranking.csv")
